210b99cf48e8fdf4220b57e26d43e65b311945d2
Timur Makarchenko <timfcsm@gmail.com>
2016-11-14 07:40:43 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin'),
+      LiveReloadPlugin  = require('webpack-livereload-plugin'),
+      path              = require('path');
+
+module.exports = {
+  context: path.resolve(__dirname, './src'),
+  
+  entry: {
+    'js/main': './js/main'
+  },
+  
+  output: {
+    path      : path.resolve(__dirname, './dist'),
+    publicPath: '/',
+    filename  : '[name].js'
+  },
+  
+  module: {
+    
+    loaders: [{
+      test   : /\.js$/,
+      exclude: /(node_modules)/,
+      loader : "babel?presets[]=es2015"
+    }, {
+      test  : /\.scss$/,
+      loader: ExtractTextPlugin.extract(
+          'style', // backup loader when not building .css file
+          'css!sass' // loaders to preprocess CSS
+      )
+    }]
+    
+  },
+  
+  plugins: [
+    new ExtractTextPlugin('styles/styles.css'),
+    new LiveReloadPlugin(options)
+  ],
+  
+  devServer: {
+    contentBase: __dirname + '/backend',
+    hot        : true
+  }
+};
+
